5010295ba9234124f550bac8ab7e92d1acb2939a
suryasaradhi <suryasaradhi3@gmail.com>
2022-03-06 14:34:04 +0530

    Refactored Alexs code

--- electronics_codebase/beamsteerer/main/main.ino
+++ electronics_codebase/beamsteerer/main/main.ino
@@ -1,16 +1,22 @@
 /*
- * Created by Suryasaradhi,Alex Shajan
- * 
- */
+   Created by Suryasaradhi,Alex Shajan
+
+*/
 
 #include <LiquidCrystal.h>
 #include <rotary.h>                 // rotary handler
-
+#include <Wire.h>                   // i2c handler
+#include <EEPROM.h>                 // memory handler
 
 //initialize pin
-const int rs = 11, en = 12, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-const int back = 9,PINA = A0,PINB = A1,PUSHB = A2;
-const int PINA2 = 6,PINB2 = 7,PUSHB2 = A3;
+#define PINA A0
+#define PINB A1
+#define PUSHB A2
+#define PINA2 6
+#define PINB2 7
+#define PUSHB2 A3
+
+const int rs = 11, en = 12, d4 = 5, d5 = 4, d6 = 3, d7 = 2, back = 9;
 bool  initial = LOW;
 
 
@@ -20,7 +26,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Rotary r = Rotary(PINA, PINB, PUSHB);        // there is no must for using interrupt pins !!
 Rotary s = Rotary(PINA2, PINB2, PUSHB2);
- 
+
 int columnsLCD = 16;
 char* MenuLine[] = {" Home", " Change User", " Option 3", " Option 4", " Option 5", " Option 6", " About"};
 int MenuItems = 7;
@@ -28,7 +34,7 @@ int CursorLine = 0;
 
 char id[5];
 char pin[4];
- 
+
 unsigned long startMillis;
 unsigned long currentMillis;
 const unsigned long period = 10000;  //the value is a number of milliseconds
@@ -37,7 +43,9 @@ const unsigned long period = 10000;  //the value is a number of milliseconds
 
 void setup ()
 {
-  
+
+
+  //rotary setups
   pinMode(PINA, INPUT_PULLUP); //RotaryCLK
   pinMode(PINB, INPUT_PULLUP); //RotaryDT
   pinMode(PUSHB, INPUT_PULLUP); //Button
@@ -45,8 +53,8 @@ void setup ()
   pinMode(PINA2, INPUT_PULLUP); //RotaryCLK
   pinMode(PINB2, INPUT_PULLUP); //RotaryDT
   pinMode(PUSHB2, INPUT_PULLUP); //Button
-  
-  
+
+
   digitalWrite (PINA, HIGH);     // enable pull-ups
   digitalWrite (PINB, HIGH);
   digitalWrite (PUSHB, HIGH);
@@ -56,21 +64,18 @@ void setup ()
   digitalWrite (PUSHB2, HIGH);
 
   pinMode(back, INPUT);
- 
-  pinMode(backlightPin, OUTPUT);          //backlightPin as an output
-  digitalWrite(backlightPin, HIGH);
- 
+
   lcd.begin (16, 2);
   lcd.clear (); // go home
   lcd.print ("Menu Master");
   lcd.setCursor(0, 1);
   lcd.print("Please Select");
-  
+
   delay(2000);
   startMillis = millis();  //initial start time
   print_menu();
 
-  
+
   Serial.begin(9600);
   Wire.begin();
 }
@@ -79,30 +84,32 @@ void setup ()
 void loop ()
 {
   volatile unsigned char result = r.process();
- 
+
   currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
   if (result) {
     startMillis = millis();;  //wake up LCD...
     result == DIR_CCW ? CursorLine = CursorLine - 1 : CursorLine = CursorLine + 1;
-    if (CursorLine < 0) { CursorLine = MenuItems - 1;} 
+    if (CursorLine < 0) {
+      CursorLine = MenuItems - 1;
+    }
     if (CursorLine > MenuItems - 1) {
       CursorLine = 0;                 // roll over to first item
     }
     print_menu();
   } //End if result
- 
+
   if (r.buttonPressedReleased(25)) {
     startMillis = millis();;     //wake up LCD...
     lcd.setCursor(0, 0);  //(col, row)
     lcd.print("You selected:");
     lcd.setCursor(0, 1);  //(col, row)
     selection();
-   // print_menu();
+    // print_menu();
   } //endif buttonPressedReleased
 } //End loop()
- 
+
 /************FUNCTIONS**************/
- 
+
 void print_menu()
 {
   lcd.clear();
@@ -113,7 +120,7 @@ void print_menu()
   lcd.setCursor(1, 1);
   lcd.print(MenuLine[CursorLine]);
 }
- 
+
 void selection()
 {
   switch (CursorLine) {
@@ -148,7 +155,7 @@ void selection()
     default:
       break;
   } //end switch
- 
+
   delay(2000);
   CursorLine = 0;     // reset to start position
 } //End selection
@@ -158,124 +165,124 @@ void homepage()
   float angle1 = 0, angle2 = 0;
   float initial_select = 0;
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print(">angle 1: " + String(angle1 ));
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(" angle 2: " + String(angle2));
-  boolean flag=true;
+  boolean flag = true;
   while (flag)
   {
     volatile unsigned char result = r.process();
     volatile unsigned char fine = s.process();
 
-if (result)
-{
-    startMillis = millis();;  //wake up LCD...
-    if (initial_select == 0)
+    if (result)
     {
-       result == DIR_CCW ? angle1 =angle1-.5 : angle1 = angle1+.5;
-    }
-    else
-    {
-       result == DIR_CCW ? angle2 =angle2-.5 : angle2 = angle2+.5;
-    }
+      startMillis = millis();;  //wake up LCD...
+      if (initial_select == 0)
+      {
+        result == DIR_CCW ? angle1 = angle1 - .5 : angle1 = angle1 + .5;
+      }
+      else
+      {
+        result == DIR_CCW ? angle2 = angle2 - .5 : angle2 = angle2 + .5;
+      }
 
-    if(angle1<0)
-    angle1=angle1+360;
-    if(angle1>360)
-    angle1=angle1-360;
-    if(angle2<0)
-    angle1=angle1+360;
-    if(angle2>360)
-    angle1=angle1-360;
-  
-  lcd.clear();
-  if (initial_select == 0)
-  {
-  lcd.setCursor(0,0);
-  lcd.print(">Angle 1: " + String(angle1 ));
-  lcd.setCursor(0,1);
-  lcd.print(" Angle 2: " + String(angle2)); 
-  }
-  
-  else
-  {
-  lcd.setCursor(0,0);
-  lcd.print(" Angle 1: " + String(angle1));
-  lcd.setCursor(0,1);
-  lcd.print(">Angle 2: " + String(angle2));
-  }
-  
-}
+      if (angle1 < 0)
+        angle1 = angle1 + 360;
+      if (angle1 > 360)
+        angle1 = angle1 - 360;
+      if (angle2 < 0)
+        angle1 = angle1 + 360;
+      if (angle2 > 360)
+        angle1 = angle1 - 360;
 
-if (fine)
-{
-    startMillis = millis();;
-    if (initial_select == 0)
-    {
-       fine == DIR_CCW ? angle1 =angle1-0.001 : angle1 = angle1 +0.001;
-    }
-    else
-    {
-       fine == DIR_CCW ? angle2 =angle2-0.001 : angle2 = angle2 +0.001;
+      lcd.clear();
+      if (initial_select == 0)
+      {
+        lcd.setCursor(0, 0);
+        lcd.print(">Angle 1: " + String(angle1 ));
+        lcd.setCursor(0, 1);
+        lcd.print(" Angle 2: " + String(angle2));
+      }
+
+      else
+      {
+        lcd.setCursor(0, 0);
+        lcd.print(" Angle 1: " + String(angle1));
+        lcd.setCursor(0, 1);
+        lcd.print(">Angle 2: " + String(angle2));
+      }
+
     }
 
-    if(angle1<0)
-    angle1=angle1+360;
-    if(angle1>360)
-    angle1=angle1-360;
-    if(angle2<0)
-    angle1=angle1+360;
-    if(angle2>360)
-    angle1=angle1-360;
-  
-  lcd.clear();
-  if (initial_select == 0)
-  {
-  lcd.setCursor(0,0);
-  lcd.print(">Angle 1: " + String(angle1 ));
-  lcd.setCursor(0,1);
-  lcd.print(" Angle 2: " + String(angle2)); 
-  }
-  
-  else
-  {
-  lcd.setCursor(0,0);
-  lcd.print(" Angle 1: " + String(angle1));
-  lcd.setCursor(0,1);
-  lcd.print(">Angle 2: " + String(angle2)); 
-  }
-  
-}
- 
-  if (r.buttonPressedReleased(25)) 
-  {
-    startMillis = millis();;
-    if (initial_select == 0)
-    initial_select = 1;
-    else
-    initial_select = 0;
+    if (fine)
+    {
+      startMillis = millis();;
+      if (initial_select == 0)
+      {
+        fine == DIR_CCW ? angle1 = angle1 - 0.001 : angle1 = angle1 + 0.001;
+      }
+      else
+      {
+        fine == DIR_CCW ? angle2 = angle2 - 0.001 : angle2 = angle2 + 0.001;
+      }
 
-    lcd.clear();
+      if (angle1 < 0)
+        angle1 = angle1 + 360;
+      if (angle1 > 360)
+        angle1 = angle1 - 360;
+      if (angle2 < 0)
+        angle1 = angle1 + 360;
+      if (angle2 > 360)
+        angle1 = angle1 - 360;
 
-    if (initial_select == 0)
-  {
-  lcd.setCursor(0,0);
-  lcd.print(">Angle 1: " + String(angle1 ));
-  lcd.setCursor(0,1);
-  lcd.print(" Angle 2: " + String(angle2)); 
+      lcd.clear();
+      if (initial_select == 0)
+      {
+        lcd.setCursor(0, 0);
+        lcd.print(">Angle 1: " + String(angle1 ));
+        lcd.setCursor(0, 1);
+        lcd.print(" Angle 2: " + String(angle2));
+      }
+
+      else
+      {
+        lcd.setCursor(0, 0);
+        lcd.print(" Angle 1: " + String(angle1));
+        lcd.setCursor(0, 1);
+        lcd.print(">Angle 2: " + String(angle2));
+      }
+
+    }
+
+    if (r.buttonPressedReleased(25))
+    {
+      startMillis = millis();;
+      if (initial_select == 0)
+        initial_select = 1;
+      else
+        initial_select = 0;
+
+      lcd.clear();
+
+      if (initial_select == 0)
+      {
+        lcd.setCursor(0, 0);
+        lcd.print(">Angle 1: " + String(angle1 ));
+        lcd.setCursor(0, 1);
+        lcd.print(" Angle 2: " + String(angle2));
+      }
+      else
+      {
+        lcd.setCursor(0, 0);
+        lcd.print(" Angle 1: " + String(angle1));
+        lcd.setCursor(0, 1);
+        lcd.print(">Angle 2: " + String(angle2));
+      }
+    }
+    if (buttonpressed())
+      break;
   }
-  else
-  {
-  lcd.setCursor(0,0);
-  lcd.print(" Angle 1: " + String(angle1));
-  lcd.setCursor(0,1);
-  lcd.print(">Angle 2: " + String(angle2));
-  }
-  }
-  if(buttonpressed())
-  break;
-} 
 }
 
 void about()
@@ -285,12 +292,12 @@ void about()
   delay(1000);
   lcd.clear();
   lcd.print("Suryasaradhi");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("B180976EP");
   delay(2000);
   lcd.clear();
   lcd.print("Alex Shajan");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("B180649EP");
   delay(2000);
 }
@@ -299,7 +306,7 @@ void login()
 {
   lcd.clear();
   lcd.print("Enter User ID:");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   UserId();
   lcd.clear();
   lcd.print("Enter password:");
@@ -311,7 +318,7 @@ bool buttonpressed()
   if (digitalRead(back) != initial)
   {
     initial = digitalRead(back);
-    if (digitalRead(back)==HIGH)
+    if (digitalRead(back) == HIGH)
       return true;
   }
   return false;
@@ -319,10 +326,10 @@ bool buttonpressed()
 
 void UserId()
 {
-  
+
 }
 
 void password()
 {
-  
+
 }
