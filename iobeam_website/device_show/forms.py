09d43896040d714b15d3923501683788fc917ddf
alexshajan <alexshajan456@gmail.com>
2022-03-31 11:37:00 +0530

    put more margin.

--- iobeam_website/device_show/forms.py
+++ iobeam_website/device_show/forms.py
@@ -3,4 +3,5 @@ from django import forms
 from django_ace import AceWidget
 
 class EditorForm(forms.Form):
-    text = forms.CharField(widget=AceWidget(mode='python', theme='twilight',width="100%",fontsize="18px",))
+    script_name = forms.CharField(label='Script name', max_length=100)
+    text = forms.CharField(label='Script', widget=AceWidget(mode='python', theme='twilight',width="100%",fontsize="18px",))
