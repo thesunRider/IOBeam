0876bae48a428f1f6f165902a737622bd209b6b6
suryasaradhi <suryasaradhi3@gmail.com>
2022-04-11 17:21:05 +0530

    Added extra ignores

--- iobeam_website/deploy_scripts/deploy_registerdevice.bat
+++ iobeam_website/deploy_scripts/deploy_registerdevice.bat
@@ -1,15 +1,22 @@
-setlocal EnableDelayedExpansions
+@echo off
+setlocal EnableDelayedExpansion
 rem 1630 - Beam Steerer
-rem 1678 - Beam Chopper
+rem 1631 - Beam Chopper
 
 
 set dev_uid=%random%%random%
-set dev_type=1630
+set dev_type=%1
 set dev_version=1.2
 set usr_email=testdev@test.com
 set usr_pass=testdev@123
 
 set host=https://projectiobeam.herokuapp.com
 
-curl -k -d "device_uid=%dev_uid%&device_type=%dev_type%&device_version=%dev_version%&email=%usr_email%&password=%usr_pass%" ^
-  -X POST %host%/polls/connector_registerdevice
+set url_fetch="device_uid=%dev_uid%&device_type=%dev_type%&device_version=%dev_version%&email=%usr_email%&password=%usr_pass%"
+echo.
+echo URL_FETCH:
+echo %url_fetch%
+echo.
+echo.
+
+curl -k -d %url_fetch% -X POST %host%/polls/connector_registerdevice
